ed292e569372dcd0e06da4bb51cf043c1b169b90
Augie Menos <augie.menos@gmail.com>
2020-12-03 14:55:14 -0700

    added endpoint display pages for header links

--- sports.js
+++ sports.js
@@ -49,33 +49,7 @@ let news = [
         "urlToImage": "https://static.clubs.nfl.com/image/private/t_editorial_landscape_12_desktop/vikings/l9er3llqtdhu13qvfr9p",
         "publishedAt": "2020-12-03T13:32:34Z",
         "content": "VIKINGS NAMED NFC PLAYER OF THE WEEK UNDER ZIMMER\r\nDefensive\r\nAnthony Barr, LB\r\n2014, Week 8, at Tampa Bay\r\nSpecial Teams\r\nAdam Thielen, WR\r\n2014, Week 13, vs. Carolina\r\nSpecial Teams\r\nMarcus Sherels… [+1433 chars]"
-    },
-    {
-        "source": {
-        "id": "cnn",
-        "name": "CNN"
-        },
-        "author": "Jasmine Wright, CNN",
-        "title": "Harris assembles staff as she builds her vice presidential portfolio - CNN",
-        "description": "Vice President-elect Kamala Harris is constructing the key team of senior staffers who will accompany her to the White House, announcing Thursday the hiring of three top roles including chief of staff.",
-        "url": "https://www.cnn.com/2020/12/03/politics/kamala-harris-staff/index.html",
-        "urlToImage": "https://cdn.cnn.com/cnnnext/dam/assets/201203131916-flournoy-kosoglu-mceldowney-split-super-tease.jpg",
-        "publishedAt": "2020-12-03T13:32:00Z",
-        "content": "(CNN)Vice President-elect Kamala Harris is constructing the key team of senior staffers who will accompany her to the White House, announcing Thursday the hiring of three top roles including chief of… [+8276 chars]"
-        },
-    {
-        "source": {
-        "id": null,
-        "name": "Chicago Tribune"
-        },
-        "author": "Eli Countryman, Variety",
-        "title": "Chicago’s John Mulaney says he was investigated by Secret Service - Chicago Tribune",
-        "description": "Chicago’s John Mulaney says he was investigated by Secret Service after ‘SNL’ joke",
-        "url": "https://www.chicagotribune.com/entertainment/ct-ent-john-mulaney-investigated-after-snl-joke-20201203-5azcx72y6jey7byy6fgnetvg6m-story.html",
-        "urlToImage": "https://www.chicagotribune.com/resizer/M0r4nL-xtYlG6vOz1cUdk1PdkZQ=/1200x0/top/cloudfront-us-east-1.images.arcpublishing.com/tronc/F2WD3N4W6ZFWRA56CQ6EPIWIFA.jpg",
-        "publishedAt": "2020-12-03T12:51:00Z",
-        "content": "John Mulaney said comments he made on Saturday Night Live in February resulted in the Secret Service opening an investigation into him.\r\nDuring an interview on Jimmy Kimmel Live! on Tuesday, the come… [+1632 chars]"
-        }];
+    }];
 
 displayStories(news)
 
@@ -104,8 +78,3 @@ function showStory(story) {
     $storyCard.append($title, $description, $image, $linkToStory)
     $divContainer.appendChild($storyCard)
 };
-
-// news.forEach((story) => {
-//     $divContainer
-//     console.log(story.source.name)
-// })
