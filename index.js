0b1c6308d0f026902ce2efc690719935d1a57a62
Augie Menos <augie.menos@gmail.com>
2020-12-07 20:04:29 -0700

    working event listener for favorite button, need to finish routing to backend

--- index.js
+++ index.js
@@ -2,26 +2,27 @@ const $divContainer = document.querySelector('.container');
 
 document.addEventListener('click', activateCarousel)
 
+let timer;
+
 function activateCarousel(event){
     if (event.target === document.getElementById("on-button")) {
         carouselCards()
         console.log("Autoscroll turned on!")
     }
     if (event.target === document.getElementById("off-button")) {
+        console.log(timer)
+        clearInterval(timer);
         console.log("Autoscroll turned off!")
     }
 }
 
 function carouselCards() {
-    setInterval(function() {
+    timer = setInterval(function() {
       const $parentContainer = document.querySelector('.container');
       const $divCard = $parentContainer.querySelectorAll('.item');
-  
-
       $divCard.forEach((card) => {
           card.classList.toggle('sliding-now');
       })
-  
       setTimeout(function() {
         $parentContainer.appendChild($divCard[0]);
       }, 5000);
@@ -31,10 +32,8 @@ function carouselCards() {
 
 fetch("http://localhost:3000/articles")
     .then(response => response.json())
-    .then(articles => displayStories(articles));
-
-
-    
+    .then(articles => displayStories(articles))
+    .then(addingEventListeners);
 
 function displayStories(story) {
     story.forEach(showStory)
@@ -66,7 +65,7 @@ function showStory(story) {
     $FavoritesButton.className = "button"
     $FavoritesButton.id = "favorites-button"
     $FavoritesButton.textContent = "Add to My Feed"
-    $FavoritesButton.onclick = function() { alert('Article added to your feed!'); };
+    
 
     const storyKeyValues = {
         title: story.title, 
@@ -77,5 +76,46 @@ function showStory(story) {
     $storyCard.append($title, $description, $image, $linkToStory, $FavoritesButton)
     $divContainer.appendChild($storyCard)
 };
+
+
+
+// const storyKeyValues = {
+//     title: story.title, 
+//     description: story.description, 
+//     link_to_image: story.urlToImage, 
+//     link_to_story: story.url }
+
+// fetch('https://localhost:3000/favorites', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify(
+//      {title: "example", description: "example", link_to_image: "example", link_to_story: "example"}),
+// })
+
+const baseURL = 'http://localhost:3000';
+const favoritesURL = `${baseURL}/favorites`;
+
+
+// fetch(favoritesURL)
+//     .then(response => response.json())
+//     .then(favorites => console.log(favorites))
+
+function addingEventListeners() {
+    const $cards = document.getElementsByClassName('item')
+
+    Array.from($cards).forEach(card => {
+        card.addEventListener('click', (event) => {
+            // console.log(event.target.parentNode);
+            const storyCardDiv = event.target.parentNode
+            const title = storyCardDiv.firstChild
+            console.log(title)
+            const description = storyCardDiv.nextChild
+            console.log(description)
+            
+        })
+    })
+}
 
 
